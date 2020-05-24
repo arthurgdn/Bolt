cc622b913b445b95ca110be4a70b0523d32abdde
Arthur Guédon <arthur.guedon@student-cs.fr>
2020-05-24 17:22:51 +0200

    Fixes sample bug

--- src/bolt.py
+++ src/bolt.py
@@ -5,7 +5,7 @@ class Server:
     def __init__(self,port):
         self.port = port
     def serve(self):
-        httpd = HTTPServer(('localhost', self.port), SimpleHTTPRequestHandler)
+        httpd = HTTPServer(('localhost', self.port), BaseHTTPRequestHandler)
         httpd.serve_forever()
 
     
